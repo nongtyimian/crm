27401e7640356d4d75e02d150a4740a5139a7d9f
Your Name <you@example.com>
2018-08-21 20:43:02 +0800

    添加的有关的方法

--- CRM/app/Http/Controllers/UserController.php
+++ CRM/app/Http/Controllers/UserController.php
@@ -8,4 +8,11 @@ use App\Http\Controllers\Controller;
 class UserController extends Controller
 {
     //
+    public function user_add(){
+        return view("index/user_add");
+    }
+    public function user_add_do(){
+        return view("index/user_add_do");
+    }
+
 }
