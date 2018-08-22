bb99decd5a6f6381ed09ff672fd8a07f4bca07ab
Your Name <you@example.com>
2018-08-22 16:53:06 +0800

    wenjing Admin Controller login展示

--- CRM/app/Http/Controllers/AdminController.php
+++ CRM/app/Http/Controllers/AdminController.php
@@ -7,5 +7,11 @@ use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-    //
+    //登录页面 温静
+    public function login( Request $request ){
+    	// echo 1212121;exit;
+
+		return view("Login/login");
+
+    }
 }
