24ce2fe4c9e41c573641779a0e64ab628eb5147c
Your Name666 <2990625115@qq.com@example.com>
2018-08-22 14:42:35 +0800

    跟单展示页面 swz

--- CRM/app/Http/Controllers/DocumentaryController.php
+++ CRM/app/Http/Controllers/DocumentaryController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class DocumentaryController extends Controller
 {
-    //
+    //跟单展示
+	public function documentary_list(){
+		return view("document/documentary_list");
+	}
 }
