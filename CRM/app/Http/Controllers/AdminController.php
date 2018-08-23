47d74d33a9d1253139659704937041c7ecf53688
Your Name <you@example.com>
2018-08-23 11:34:13 +0800

    wenjing 编辑器崩了的提交

--- CRM/app/Http/Controllers/AdminController.php
+++ CRM/app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
 
 class AdminController extends Controller
 {
@@ -14,4 +16,62 @@ class AdminController extends Controller
 		return view("Login/login");
 
     }
+
+//构造方法    
+    // public function __construct(){
+    //     $this->middleware(function ($request, $next) {
+    //         $user_data = $request->session() -> get('user_data');
+    //         if(empty($user_data)) {
+    //             redirect('/login')->send();
+    //         }
+    //         return $next($request);
+    //     });
+    // }
+
+
+    //登录提交 温静
+    public function login_do( Request $request ){
+//        echo 1212121212;exit;
+        //密码格式MD5 （ MD5 （密码）. MD5（盐））；
+//        echo md5(md5('root').md5(123951));exit;
+    	$data = $request -> post();
+
+        if( empty( $data['account'] ) ){
+            return [ 'msg' => '账号不能为空' , 'code' => '2' ];
+        }
+        if( empty( $data['pwsds'] ) ){
+            return [ 'msg' => '密码不能为空' , 'code' => '2' ];
+        }
+
+        $crm_admin_get = $res = DB::table('crm_admin') -> where( 'admin_name' , '=' , $data['account'] ) -> first();
+
+        if( empty( $crm_admin_get ) ){
+            return[ 'msg'=> '账号或密码错误1' , 'code' => '2' ];
+        }
+        print_r($data->salt);exit;
+        if( empty( $data->salt ) ){
+            return[ 'msg'=> '账号或密码错误2' , 'code' => '2' ];
+        }
+        $pwd = md5( md5( $data['pwsds'] ).md5( $data['salt'] ) );
+
+        if( $pwd != $crm_admin_get['pwd']){
+
+            return[ 'msg'=> '账号或密码错误3' , 'code' => '2' ];
+        }
+
+
+
+//        $where = [
+//            'crm_admin' => $data['account'],
+//            'pwd' => md5( md5( $data['pwsds'] ).md5( $data['salt'] ) ) ,
+//        ];
+////        $pwd_get = $res = DB::table('crm_admin') -> where( $where ) -> first();
+//        var_dump($pwd_get);exit;
+
+
+        var_dump($crm_admin_get);exit;
+
+    	print_r($data);exit;
+    }
+
 }
--- CRM/resources/views/Login/login.blade.php
+++ CRM/resources/views/Login/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>登录</title>
+	<title>crm4管理系统登录</title>
 	<meta name="renderer" content="webkit|ie-comp|ie-stand">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width,user-scalable=yes, minimum-scale=0.4, initial-scale=0.8,target-densitydpi=low-dpi" />
@@ -19,13 +19,14 @@
 <body class="login-bg">
     
     <div class="login layui-anim layui-anim-up">
-        <div class="message">x-admin2.0-管理登录</div>
+        <div class="message">crm4管理系统登录</div>
         <div id="darkbannerwrap"></div>
         
-        <form method="post" class="layui-form" >
-            <input name="username" placeholder="用户名"  type="text" lay-verify="required" class="layui-input" >
+        <form class="layui-form" >
+            @csrf
+            <input name="account" placeholder="用户名"  type="text" lay-verify="required" class="layui-input" >
             <hr class="hr15">
-            <input name="password" lay-verify="required" placeholder="密码"  type="password" class="layui-input">
+            <input name="pwsds" lay-verify="required" placeholder="密码"  type="password" class="layui-input">
             <hr class="hr15">
             <input value="登录" lay-submit lay-filter="login" style="width:100%;" type="submit">
             <hr class="hr20" >
@@ -42,10 +43,36 @@
               //监听提交
               form.on('submit(login)', function(data){
                 // alert(888)
-                layer.msg(JSON.stringify(data.field),function(){
-                    location.href='index.html'
-                });
-                return false;
+                  $.ajax({
+                      type: 'post',
+                      dataType: "json",
+                      data: data.field,
+                      url: "/login_do",
+                      success: function (datas) {
+                          console.log(data);
+                          if (datas.code == 1) {
+//                              layer.msg(datas.msg, {icon: datas.code, time: 1500}, function () {
+//
+//                                  location.href = "/login";
+//                              });
+                          } else {
+                              layer.msg(datas.msg, {icon: datas.code});
+                          }
+                      }
+
+                  })
+                  return false;
+
+
+
+
+
+
+//
+//                layer.msg(JSON.stringify(data.field),function(){
+//                    location.href='index.html'
+//                });
+//                return false;
               });
             });
         })
