afba4d30ba1b7fcac29cd52d1d6766409dc47310
Your Name666 <2990625115@qq.com@example.com>
2018-08-23 08:28:29 +0800

    跟单表控制器swz

--- CRM/app/Http/Controllers/DocumentaryController.php
+++ CRM/app/Http/Controllers/DocumentaryController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +9,10 @@ class DocumentaryController extends Controller
 {
     //跟单展示
 	public function documentary_list(){
-		return view("document/documentary_list");
+		$res = DB::table('crm_dym')->paginate(1);
+		$count=DB::table("crm_dym")->count();
+		
+		
+		return view("document/documentary_list",["res"=>$res,'count'=>$count]);
 	}
 }
