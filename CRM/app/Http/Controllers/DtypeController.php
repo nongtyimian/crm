70b41e21f1da81de45dfcebedb34d671c596d595
Your Name666 <2990625115@qq.com@example.com>
2018-08-24 14:30:09 +0800

    跟单类型

--- CRM/app/Http/Controllers/DtypeController.php
+++ CRM/app/Http/Controllers/DtypeController.php
@@ -1,11 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DtypeController extends Controller
 {
-    //
+    //添加类型
+	public function dtype_add(){
+		return view("document/dtype");	
+	}
+
+	public function dtype_add_do(){
+		$data=$_GET;
+		$res=DB::table("dym_type")->insert($data);
+		if($res){
+			return 1;
+		}
+	}
 }
